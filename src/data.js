e14e1f010362056204a52009e747f3bfe09fd61e
Chris Atkin <atkinchris@gmail.com>
2018-01-25 14:56:23 +0000

    Add labels to end stops

--- src/data.js
+++ src/data.js
@@ -1,11 +1,43 @@
-export default [
-  { type: 'endcap', x: 10, y: 10 },
-  { type: 'stop', x: 10, y: 15, orientation: 'left' },
-  { type: 'stop', x: 10, y: 20 },
-  { type: 'point', x: 10, y: 25 },
-  { type: 'corner', x: 10, y: 30 },
-  { type: 'stop', x: 15, y: 30, orientation: 'up' },
-  { type: 'station', x: 20, y: 30 },
-  { type: 'stop', x: 25, y: 30, orientation: 'down' },
-  { type: 'endcap', x: 30, y: 30, orientation: 'vertical' },
-]
+export default [{
+  type: 'endcap',
+  x: 10,
+  y: 10,
+  label: 'Entrance',
+}, {
+  type: 'stop',
+  x: 10,
+  y: 15,
+  orientation: 'left',
+}, {
+  type: 'stop',
+  x: 10,
+  y: 20,
+}, {
+  type: 'point',
+  x: 10,
+  y: 25,
+}, {
+  type: 'corner',
+  x: 10,
+  y: 30,
+}, {
+  type: 'stop',
+  x: 15,
+  y: 30,
+  orientation: 'up',
+}, {
+  type: 'station',
+  x: 20,
+  y: 30,
+}, {
+  type: 'stop',
+  x: 25,
+  y: 30,
+  orientation: 'down',
+}, {
+  type: 'endcap',
+  x: 30,
+  y: 30,
+  orientation: 'vertical',
+  label: 'Output',
+}]
--- src/elements/endCap.js
+++ src/elements/endCap.js
@@ -1,12 +1,21 @@
 import { SIZE_UNIT } from '../constants'
 
-const buildEndCap = (group, { x, y, orientation = 'horizontal' }) => {
+const buildEndCap = (group, { x, y, orientation = 'horizontal', label }) => {
   const endCap = group.rect(SIZE_UNIT * 3, SIZE_UNIT)
 
   endCap.move(x - (SIZE_UNIT * 1.5), y - (SIZE_UNIT / 2))
 
   if (orientation === 'vertical') {
     endCap.rotate(90)
+  }
+
+  if (label) {
+    const labelElement = group.text(label)
+    labelElement.move(x + SIZE_UNIT, y - (SIZE_UNIT * 1.2))
+
+    if (orientation === 'horizontal') {
+      labelElement.dx(SIZE_UNIT)
+    }
   }
 
   return endCap
