698c1ae7837aa953e262a647ae4a38597f7f7f7e
Chris Atkin <atkinchris@gmail.com>
2018-01-25 10:06:17 +0000

    Rename stations to stops

--- map.js
+++ map.js
@@ -1,34 +1,34 @@
 const SIZE_UNIT = 8
 const mapData = [
-  { type: 'station', x: 100, y: 100 },
-  { type: 'station', x: 100, y: 150, orientation: 'left' },
-  { type: 'station', x: 100, y: 200 },
-  { x: 100, y: 250 },
-  { type: 'station', x: 150, y: 250, orientation: 'up' },
-  { type: 'station', x: 200, y: 250, orientation: 'down' },
+  { type: 'stop', x: 100, y: 100 },
+  { type: 'stop', x: 100, y: 150, orientation: 'left' },
+  { type: 'stop', x: 100, y: 200 },
+  { type: 'corner', x: 100, y: 250 },
+  { type: 'stop', x: 150, y: 250, orientation: 'up' },
+  { type: 'stop', x: 200, y: 250, orientation: 'down' },
 ]
 
-const buildStation = (map, x, y, orientation = 'right') => {
-  const station = map.rect(SIZE_UNIT * 2, SIZE_UNIT).move(x, y)
+const buildStop = (map, x, y, orientation = 'right') => {
+  const stop = map.rect(SIZE_UNIT * 2, SIZE_UNIT).move(x, y)
 
   switch (orientation) {
     case 'right':
-      station.dx(-SIZE_UNIT / 2)
+      stop.dx(-SIZE_UNIT / 2)
       break
     case 'left':
-      station.dx(-SIZE_UNIT / 2).rotate(180, x)
+      stop.dx(-SIZE_UNIT / 2).rotate(180, x)
       break
     case 'up':
-      station.dy(-SIZE_UNIT / 2).rotate(-90, x, y + (SIZE_UNIT / 2))
+      stop.dy(-SIZE_UNIT / 2).rotate(-90, x, y + (SIZE_UNIT / 2))
       break
     case 'down':
-      station.dy(-SIZE_UNIT / 2).rotate(90, x, y - (SIZE_UNIT / 2))
+      stop.dy(-SIZE_UNIT / 2).rotate(90, x, y - (SIZE_UNIT / 2))
       break
     default:
       break
   }
 
-  return station
+  return stop
 }
 
 const buildMap = () => {
@@ -37,8 +37,8 @@ const buildMap = () => {
   const linePoints = []
 
   mapData.forEach(({ type, x, y, orientation }) => {
-    if (type === 'station') {
-      buildStation(map, x, y, orientation)
+    if (type === 'stop') {
+      buildStop(map, x, y, orientation)
     }
 
     linePoints.push([x, y])
