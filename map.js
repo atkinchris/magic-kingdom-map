778357e9ed68198edf729457ceb5c1ce1360f7d2
Chris Atkin <atkinchris@gmail.com>
2018-01-25 09:55:47 +0000

    Add station builder

--- map.js
+++ map.js
@@ -1,13 +1,20 @@
 const SIZE_UNIT = 8
 
 const buildStation = (map, x, y, orientation = 'right') => {
-  const station = map
-    .rect(SIZE_UNIT * 2, SIZE_UNIT)
-    .move(x - (SIZE_UNIT / 2), y - SIZE_UNIT)
+  const station = map.rect(SIZE_UNIT * 2, SIZE_UNIT).move(x, y)
 
   switch (orientation) {
     case 'right':
-      station.dx(0)
+      station.dx(-SIZE_UNIT / 2)
+      break
+    case 'left':
+      station.dx(-SIZE_UNIT / 2).rotate(180, x)
+      break
+    case 'up':
+      station.dy(-SIZE_UNIT / 2).rotate(-90, x, y + (SIZE_UNIT / 2))
+      break
+    case 'down':
+      station.dy(-SIZE_UNIT / 2).rotate(90, x, y - (SIZE_UNIT / 2))
       break
     default:
       break
@@ -19,11 +26,13 @@ const buildStation = (map, x, y, orientation = 'right') => {
 const buildMap = () => {
   const map = SVG('map')
 
-  map.polyline([[100, 100], [100, 150], [100, 200]]).fill('none').stroke({ width: SIZE_UNIT })
+  map.polyline([[100, 100], [100, 150], [100, 200], [100, 250], [150, 250], [200, 250]]).fill('none').stroke({ width: SIZE_UNIT }).opacity(0.5)
 
   buildStation(map, 100, 100)
-  buildStation(map, 100, 150)
+  buildStation(map, 100, 150, 'left')
   buildStation(map, 100, 200)
+  buildStation(map, 150, 250, 'up')
+  buildStation(map, 200, 250, 'down')
 }
 
 window.buildMap = buildMap
