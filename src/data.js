c53ff354a950f38fe3ce8cafcb4ea07863056392
Chris Atkin <atkinchris@gmail.com>
2018-01-25 19:27:50 +0000

    Add second line

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default {
+export default [{
   name: 'Main Street, USA',
   color: 'red',
   points: [{
@@ -73,4 +73,110 @@ export default {
       alignment: 'below',
     },
   }],
-}
+}, {
+  name: 'Tomorrowland',
+  color: 'blue',
+  points: [{
+    type: 'station',
+    x: 30,
+    y: 10,
+    label: {
+      text: 'Cinderella Castle',
+      alignment: 'above',
+    },
+  }, {
+    type: 'point',
+    x: 40,
+    y: 10,
+  }, {
+    type: 'corner',
+    x: 45,
+    y: 10,
+  }, {
+    type: 'point',
+    x: 45,
+    y: 15,
+  }, {
+    type: 'corner',
+    x: 45,
+    y: 20,
+  }, {
+    type: 'stop',
+    x: 50,
+    y: 20,
+    orientation: 'down',
+    label: {
+      text: 'Tomorrowland Terrace',
+      alignment: 'below',
+    },
+  }, {
+    type: 'stop',
+    x: 60,
+    y: 20,
+    orientation: 'up',
+    label: {
+      text: 'Monster\'s Inc Laugh Floor',
+      alignment: 'above',
+    },
+  }, {
+    type: 'stop',
+    x: 65,
+    y: 20,
+    orientation: 'down',
+    label: {
+      text: 'Buzz Lightyear Space Ranger Spin',
+      alignment: 'below',
+    },
+  }, {
+    type: 'stop',
+    x: 70,
+    y: 20,
+    orientation: 'up',
+    label: {
+      text: 'Astro Orbit People Mover',
+      alignment: 'above',
+    },
+  }, {
+    type: 'corner',
+    x: 80,
+    y: 20,
+  }, {
+    type: 'stop',
+    x: 80,
+    y: 10,
+    orientation: 'left',
+    label: {
+      text: 'Carousel of Progress',
+      alignment: 'left',
+    },
+  }, {
+    type: 'stop',
+    x: 80,
+    y: 5,
+    orientation: 'left',
+    label: {
+      text: 'Space Mountain',
+      alignment: 'left',
+    },
+  }, {
+    type: 'stop',
+    x: 80,
+    y: 0,
+    orientation: 'left',
+    label: {
+      text: 'Tomorrowland Speedway',
+      alignment: 'left',
+    },
+  }, {
+    type: 'stop',
+    x: 80,
+    y: -5,
+    orientation: 'left',
+    label: {
+      text: 'Cosmic Starlight Cafe',
+      alignment: 'left',
+    },
+  }, {
+    // WDW Railroad
+  }],
+}]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import SVG from 'svg.js'
 
-import data from './data'
+import lines from './data'
 import { buildLine } from './elements'
 
 const map = SVG('map').group()
 
-buildLine(map, data)
+lines.forEach(line => buildLine(map, line))
+
+map.dmove(0, 400)
