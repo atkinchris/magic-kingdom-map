4b9a97fb52d8a86a10937689cf442193978da125
Chris Atkin <atkinchris@gmail.com>
2018-01-25 14:44:57 +0000

    Fix end cap sizing

--- src/elements/endCap.js
+++ src/elements/endCap.js
@@ -1,7 +1,9 @@
 import { SIZE_UNIT } from '../constants'
 
 const buildEndCap = (group, { x, y, orientation = 'horizontal' }) => {
-  const endCap = group.rect(SIZE_UNIT * 2, SIZE_UNIT).move(x - SIZE_UNIT, y - (SIZE_UNIT / 2))
+  const endCap = group.rect(SIZE_UNIT * 3, SIZE_UNIT)
+
+  endCap.move(x - (SIZE_UNIT * 1.5), y - (SIZE_UNIT / 2))
 
   if (orientation === 'vertical') {
     endCap.rotate(90)
