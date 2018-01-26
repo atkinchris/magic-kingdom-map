4f121dddbae57853cc0c7f09af7b7b55ed8f318b
Chris Atkin <atkinchris@gmail.com>
2018-01-26 07:03:20 +0000

    Hack viewport bounds

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import SVG from 'svg.js'
 
 import lines from './data'
 import { buildLine } from './elements'
+import { SIZE_UNIT } from './constants'
 
-const map = SVG('map').group()
+const svg = SVG('map')
+const map = svg.group()
+
+const allPoints = lines.reduce((out, { points }) => [...out, ...points], [])
+const bounds = allPoints.reduce((out, { x, y }) => {
+  /* eslint-disable no-param-reassign */
+  if (x < out.left) out.left = x
+  if (x > out.right) out.right = x
+  if (y > out.bottom) out.bottom = y
+  if (y < out.top) out.top = y
+  /* eslint-enable no-param-reassign */
+
+  return out
+}, { left: 0, right: 0, top: 0, bottom: 0 })
 
 lines.reverse().forEach(line => buildLine(map, line))
 
-map.dmove(600, 400)
+const width = ((-bounds.left + bounds.right) * SIZE_UNIT) + (SIZE_UNIT * 25)
+const height = ((-bounds.top + bounds.bottom) * SIZE_UNIT) + (SIZE_UNIT * 20)
+
+svg.size(width, height)
+map.dmove(-bounds.left * SIZE_UNIT, -bounds.top * SIZE_UNIT)
+map.dmove(SIZE_UNIT * 12, SIZE_UNIT * 10)
