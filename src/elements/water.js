a39fd59eb8aaa5d05d255a8f3b46b8411428dffc
Chris Atkin <atkinchris@gmail.com>
2018-01-26 15:14:25 +0000

    Simplify water

--- src/elements/water.js
+++ src/elements/water.js
@@ -2,63 +2,52 @@ import { SIZE_UNIT } from '../constants'
 
 const RADIUS = 2.5 * SIZE_UNIT
 
-const buildWater = (map, { shapes }) => {
-  const group = map.group()
-  const water = group.rect(10000, 10000).fill('#c6ebfc').center(0, 0)
-  const mask = group.mask().fill('grey').stroke({ width: SIZE_UNIT / 3, color: 'white' })
+const buildWater = (map, rawPoints) => {
+  const points = rawPoints.map(([x, y]) => [x * SIZE_UNIT, y * SIZE_UNIT])
+  const path = points.reduce((out, [x, y], index, arr) => {
+    const [previousX, previousY] = arr[index - 1] || arr[arr.length - 1]
+    const [nextX, nextY] = arr[index + 1] || arr[0]
 
-  shapes.forEach(({ points: rawPoints, subtract }) => {
-    const points = rawPoints.map(([x, y]) => [x * SIZE_UNIT, y * SIZE_UNIT])
-    const path = points.reduce((out, [x, y], index, arr) => {
-      const [previousX, previousY] = arr[index - 1] || arr[arr.length - 1]
-      const [nextX, nextY] = arr[index + 1] || arr[0]
-
-      if (index === 0) {
-        out.push(`M${x + RADIUS} ${y}`)
-        return out
-      }
-
-      if (previousY === y && nextX === x) {
-        if (previousX < x) {
-          out.push(`L${x - RADIUS} ${y}`)
-        } else {
-          out.push(`L${x + RADIUS} ${y}`)
-        }
-
-        if (nextY < y) {
-          out.push(`Q${x} ${y} ${x} ${y - RADIUS}`)
-        } else {
-          out.push(`Q${x} ${y} ${x} ${y + RADIUS}`)
-        }
-      }
-
-      if (previousX === x && nextY === y) {
-        if (previousY < y) {
-          out.push(`L${x} ${y - RADIUS}`)
-        } else {
-          out.push(`L${x} ${y + RADIUS}`)
-        }
-
-        if (nextX < x) {
-          out.push(`Q${x} ${y} ${x - RADIUS} ${y}`)
-        } else {
-          out.push(`Q${x} ${y} ${x + RADIUS} ${y}`)
-        }
-      }
-
+    if (index === 0) {
+      out.push(`M${x + RADIUS} ${y}`)
       return out
-    }, []).join(' ')
-
-    const polygon = group.path(path)
-
-    if (subtract) {
-      polygon.fill('black')
     }
 
-    mask.add(polygon)
-  })
+    if (previousY === y && nextX === x) {
+      if (previousX < x) {
+        out.push(`L${x - RADIUS} ${y}`)
+      } else {
+        out.push(`L${x + RADIUS} ${y}`)
+      }
 
-  water.maskWith(mask)
+      if (nextY < y) {
+        out.push(`Q${x} ${y} ${x} ${y - RADIUS}`)
+      } else {
+        out.push(`Q${x} ${y} ${x} ${y + RADIUS}`)
+      }
+    }
+
+    if (previousX === x && nextY === y) {
+      if (previousY < y) {
+        out.push(`L${x} ${y - RADIUS}`)
+      } else {
+        out.push(`L${x} ${y + RADIUS}`)
+      }
+
+      if (nextX < x) {
+        out.push(`Q${x} ${y} ${x - RADIUS} ${y}`)
+      } else {
+        out.push(`Q${x} ${y} ${x + RADIUS} ${y}`)
+      }
+    }
+
+    return out
+  }, []).join(' ')
+
+  map
+    .path(path)
+    .fill('#c6ebfc')
+    .opacity(0.5)
 }
 
 export default buildWater
