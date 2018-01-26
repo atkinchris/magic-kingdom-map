844ca72cf59d82a2d882cedbb2696169d7d534c3
Chris Atkin <atkinchris@gmail.com>
2018-01-26 10:05:41 +0000

    Improve water boolean operations

--- src/elements/water.js
+++ src/elements/water.js
@@ -1,8 +1,21 @@
 import { SIZE_UNIT } from '../constants'
 
-const buildWater = (map, { points: rawPoints }) => {
-  const points = rawPoints.map(p => p * SIZE_UNIT)
-  map.polygon(points).fill('#c6ebfc').stroke({ width: 1, color: '#00aff2' })
+const buildWater = (map, { shapes }) => {
+  const group = map.group()
+  const water = group.rect(10000, 10000).fill('#c6ebfc').center(0, 0)
+  const mask = group.mask().fill('grey').stroke({ width: SIZE_UNIT / 2, color: 'white' })
+
+  shapes.forEach(({ points, subtract }) => {
+    const polygon = group.polygon(points.map(p => p * SIZE_UNIT))
+
+    if (subtract) {
+      polygon.fill('black')
+    }
+
+    mask.add(polygon)
+  })
+
+  water.maskWith(mask)
 }
 
 export default buildWater
--- src/index.js
+++ src/index.js
@@ -27,4 +27,4 @@ const height = ((-bounds.top + bounds.bottom) * SIZE_UNIT) + (SIZE_UNIT * 20)
 
 svg.size(width, height)
 map.dmove(-bounds.left * SIZE_UNIT, -bounds.top * SIZE_UNIT)
-map.dmove(SIZE_UNIT * 12, SIZE_UNIT * 10)
+map.dmove(SIZE_UNIT * 12, SIZE_UNIT * 12)
